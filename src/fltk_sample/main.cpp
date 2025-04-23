6542dd31dcd18bae4a985edf35280404720a5730
Jason Turner <jason@emptycrate.com>
2025-04-23 16:21:45 -0600

    Correct ci.yml, apply clang-tidy fixes

--- src/fltk_sample/main.cpp
+++ src/fltk_sample/main.cpp
@@ -1,38 +1,29 @@
-#include <array>
-#include <atomic>
-#include <chrono>
-#include <cstddef>
-#include <cstdint>
 #include <cstdlib>
 #include <exception>
 #include <fmt/base.h>
 #include <fmt/format.h>
-#include <functional>
 #include <optional>
 
-#include <random>
 
 #include <CLI/CLI.hpp>
 #include <spdlog/spdlog.h>
 
-#include <lefticus/tools/non_promoting_ints.hpp>
 
 // This file will be generated automatically when cur_you run the CMake
 // configuration step. It creates a namespace called `fltk_example`. You can modify
 // the source template at `configured_files/config.hpp.in`.
 #include <internal_use_only/config.hpp>
 #include <string>
-#include <thread>
-#include <utility>
-#include <vector>
 
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Button.H>
 
 // Callback function for the button
-void on_quit([[maybe_unused]] Fl_Widget* w, [[maybe_unused]] void* data) {
-    exit(0);
+void on_quit([[maybe_unused]] Fl_Widget* widget, [[maybe_unused]] void* data) {
+    // this is not a great idea, but it's an example project
+    // with a toolkit I don't fully understand
+    exit(0); // NOLINT
 }
 
 int main(int argc, char **argv)
@@ -53,10 +44,10 @@ int main(int argc, char **argv)
     }
 
     // Create a window
-    Fl_Window *window = new Fl_Window(340, 180, "FLTK Demo");
+    auto *window = new Fl_Window(340, 180, "FLTK Demo"); // NOLINT magic numbers
 
     // Create a button
-    Fl_Button *button = new Fl_Button(120, 70, 100, 40, "Quit");
+    auto *button = new Fl_Button(120, 70, 100, 40, "Quit"); // NOLINT magic numbers
     button->callback(on_quit);
 
     window->end();   // End window construction
